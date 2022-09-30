09c0781a8b0dbf85501bf372b3f77aa3db18c639
Taylor johnson <taylorjohnsondev@gmail.com>
2022-09-29 20:43:00 -0400

    Taylor and Brooks Connect 4 WIP

--- code.js
+++ code.js
@@ -1,3 +1,30 @@
-// Your Code Here.
+//Players
+let player1 = "Red";
+let player2 = "Black";
+let currentPlayer = player1;
+let board = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+]
 
+function renderBoard(){
+    for (let i = 0; i < 42; i++){
+       let gameTile = document.createElement("div")
+        gameTile.classList.add("column")
 
+        gameTile.addEventListener("click", function(){
+           if (currentPlayer == player1){
+            gameTile.classList.add("red-piece");
+         }
+        else if (currentPlayer == player2){
+            gameTile.classList.add("black-piece")
+        }
+        })
+        document.getElementById("board").append(gameTile)
+    }
+}
+renderBoard()
