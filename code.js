5592bf8039c15487de5e2d05929e5a4a6fc8945e
Taylor johnson <taylorjohnsondev@gmail.com>
2022-09-29 21:01:24 -0400

    Taylor and Brooks C4 WIP

--- code.js
+++ code.js
@@ -1,30 +1,31 @@
 //Players
-let player1 = "Red";
-let player2 = "Black";
-let currentPlayer = player1;
+let player1 = 1;
+let player2 = 2;
+let currentPlayer = 0;
 let board = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-]
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+];
 
-function renderBoard(){
-    for (let i = 0; i < 42; i++){
-       let gameTile = document.createElement("div")
-        gameTile.classList.add("column")
+function renderBoard() {
+  for (let i = 0; i < 42; i++) {
+    let gameTile = document.createElement("div");
+    gameTile.classList.add("column");
 
-        gameTile.addEventListener("click", function(){
-           if (currentPlayer == player1){
-            gameTile.classList.add("red-piece");
-         }
-        else if (currentPlayer == player2){
-            gameTile.classList.add("black-piece")
-        }
-        })
-        document.getElementById("board").append(gameTile)
-    }
+    gameTile.addEventListener("click", function () {
+      currentPlayer += 1;
+      if (currentPlayer == player1) {
+        gameTile.classList.add("red-piece");
+      } else if (currentPlayer == player2) {
+        gameTile.classList.add("black-piece");
+        currentPlayer = 0;
+      }
+    });
+    document.getElementById("board").append(gameTile);
+  }
 }
-renderBoard()
+renderBoard();
